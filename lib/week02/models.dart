6a7da40b7c46d345848a2acf4d6d1f1e68e5942c
Bekzat Zhumamurat <BekaZhumamurat77@gmail.com>
2026-09-10 18:35:11 +0600

    data

--- lib/week02/models.dart
+++ lib/week02/models.dart
@@ -1,33 +1,3 @@
-class Author {
-  final String name;
-  final String? country;
-
-  const Author({
-    required this.name,
-    this.country,
-  });
-
-  @override
-  String toString() => '$name (${country ?? 'unknown'})';
-}
 
 
 
-
-enum Genre {
-  craft('Craft'),
-  theory('Theory'),
-  unknown('Unknown');
-
-  final String label;
-
-  const Genre(this.label);
-
-  factory Genre.fromString(String? raw) {
-    return switch (raw) {
-      'craft' => Genre.craft,
-      'theory' => Genre.theory,
-      _ => Genre.unknown,
-    };
-  }
-}
